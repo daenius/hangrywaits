b3d9deb5e7112ff1c54ee3ad3acd9341512fed30
Dennis Zhu <zhu.dennis@gmail.com>
2015-04-05 23:23:16 -0700

    Search Panel Improvements
    * Improved Search button
    * Improved panel show/hide animation
    * Added hint to EditText for Search box

--- app/src/main/java/com/boolong/hangrywaits/Home.java
+++ app/src/main/java/com/boolong/hangrywaits/Home.java
@@ -23,6 +23,8 @@ import com.boolong.hangrywaits.dataprovider.DataProvider;
 
 import java.util.List;
 
+import at.markushi.ui.CircleButton;
+
 
 public class Home extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -143,7 +145,7 @@ public class Home extends ActionBarActivity
                     .getFavorites();
 
             ListView homeListView = (ListView) rootView.findViewById(R.id.home_list_view);
-            final ImageButton searchButton = (ImageButton) rootView.findViewById(R.id.search_button);
+            final CircleButton searchButton = (CircleButton) rootView.findViewById(R.id.search_button);
 
             HomeListAdapter arrayAdapter =
                     new HomeListAdapter(this.getActivity(), R.layout.home_list_item, stringsToPopulate);
@@ -165,15 +167,17 @@ public class Home extends ActionBarActivity
                 @Override
                 public void onClick(View v) {
                     final View searchPanel = v.getRootView().findViewById(R.id.search_view);
-                    ObjectAnimator animateX = ObjectAnimator.ofFloat(searchPanel, "translationX", v.getRootView().getRight(), v.getRootView().getLeft());
-                    ObjectAnimator animateY = ObjectAnimator.ofFloat(searchPanel, "translationY", v.getRootView().getBottom(), v.getRootView().getTop());
-                    animateX.setDuration(350);
-                    animateY.setDuration(350);
+                    ObjectAnimator animateX;
+                    ObjectAnimator animateY;
                     if (searchPanel.getVisibility() == View.GONE) {
+                        animateX = ObjectAnimator.ofFloat(searchPanel, "translationX", v.getRootView().getRight() - v.getWidth()/2, v.getRootView().getLeft());
+                        animateY = ObjectAnimator.ofFloat(searchPanel, "translationY", v.getRootView().getBottom() - v.getHeight()/2, v.getRootView().getTop());
+                        animateX.setDuration(350);
+                        animateY.setDuration(350);
                         searchPanel.setVisibility(View.VISIBLE);
                     } else {
-                        animateX = ObjectAnimator.ofFloat(searchPanel, "translationX", v.getRootView().getLeft(), v.getRootView().getRight());
-                        animateY = ObjectAnimator.ofFloat(searchPanel, "translationY", v.getRootView().getTop(), v.getRootView().getBottom());
+                        animateX = ObjectAnimator.ofFloat(searchPanel, "translationX", v.getRootView().getLeft(), v.getRootView().getRight() - v.getWidth()/2);
+                        animateY = ObjectAnimator.ofFloat(searchPanel, "translationY", v.getRootView().getTop(), v.getRootView().getBottom() - v.getHeight()/2);
                         animateX.setDuration(200);
                         animateY.setDuration(200);
                         animateX.addListener(new AnimatorListenerAdapter() {
